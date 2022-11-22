cc199badb2dd48f08db887069571a3b2081e6f1f
EmbersAdriftGuide <116581507+EmbersAdriftGuide@users.noreply.github.com>
2022-11-22 18:28:44 +0000

    Add files via upload
    front page specs accordions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 function checkRadio(name) {
-    document.getElementById(name).checked = true; window.scrollTo(0,0);
+  if (name.match("tabJuggernaut") || name.match("tabKnight") || name.match("tabMarshal")) {
+    document.getElementById("tabDefender").checked = true; window.scrollTo(0,0)
+  } else if (name.match("tabBerserker") || name.match("tabBrigand") || name.match("tabWarden")) {
+    document.getElementById("tabStriker").checked = true; window.scrollTo(0,0)
+  } else if (name.match("tabDuelist") || name.match("tabSentinel") || name.match("tabWarlord")) {
+    document.getElementById("tabSupport").checked = true; window.scrollTo(0,0)
+  }
+  document.getElementById(name).checked = true;
 };
     
 function openOverlay(name) {
@@ -11,4 +18,13 @@ function closeOverlay() {
     for (i = 0; i < overlayList.length; i++) {
       overlayList[i].style.display = "none";
     }
+  }
+
+  function accordion(name) {
+    var blah = document.getElementById(name)
+        if (blah.style.display === "block") {
+            blah.style.display = "none";
+        } else {
+            blah.style.display = "block";
+        }
   }
