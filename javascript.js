8fdd5a24b0b00d516e36fdb3027a55456237d606
EmbersAdriftGuide <116581507+EmbersAdriftGuide@users.noreply.github.com>
2022-11-29 20:58:38 +0000

    Add files via upload
    skill updates for warden / jugg dots

--- javascript.js
+++ javascript.js
@@ -20,11 +20,49 @@ function closeOverlay() {
     }
   }
 
-  function accordion(name) {
-    var blah = document.getElementById(name)
-        if (blah.style.display === "block") {
-            blah.style.display = "none";
-        } else {
-            blah.style.display = "block";
-        }
-  }
+function accordion(name) {
+  var doc = document.getElementById(name) 
+    if (doc.style.display === "block") {
+        doc.style.display = "none";
+    } else {
+        doc.style.display = "block";
+    }
+    
+}
+
+var recipe = {Name:"", Materials:{}}
+
+function crafting(item, component, value) {
+  recipe.Name = item
+  recipe.Materials[component] = value
+  var data = JSON.stringify(filterRecipe())
+  var doc = document.getElementById(item + "Output")
+  document.getElementById(component + item + "Btn").innerHTML = value;
+  document.getElementById(component + item).style.display = "none";
+    if (filterRecipe().length > 1){
+        doc.innerHTML = "<p>Please select materials from the lists above</p>";
+    } else if (filterRecipe().length === 0){
+        doc.innerHTML = "<p>No data available, please select different materials</p>";
+    } else {
+//        doc.innerHTML = "<p>" + data + "</p>"; 
+//   }
+    for (const [key, value] of Object.entries(filterRecipe())) {
+      doc.innerHTML = "<p>" + key + " :" + value + "</p>";
+      console.log(key, value);
+    }}
+  console.log(filterRecipe(armorsmith, recipe));
+}
+
+function filterRecipe(profession, recipeParam) {
+  let out = profession[recipeParam.Name]
+  for(let i of Object.entries(recipeParam.Materials)){
+    out = out.filter(r => r[i[0]] === i[1])}
+  return out
+}
+
+function loadJSON(file) {
+  let scriptEle = document.createElement("script");
+  scriptEle.setAttribute("src", file);
+  scriptEle.setAttribute("type", "text/javascript");
+  document.body.appendChild(scriptEle);
+}
